1a185f68da6eee77b98ff8fc8a678dd791ecb2e3
Jak Major <hellojakkaiser@gmail.com>
2021-09-07 21:23:03 -0700

    commit tree 254933316e2a083f8c69795d2ce78c75070acbe4

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-// Add your code here
+const formData = {
+  dogName: "Byron",
+  dogBreed: "Poodle",
+};
+
+const configurationObject = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+fetch(" http://localhost:3000/dogs", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (object) {
+    console.log(object);
+  })
+  .catch(function (error) {
+    alert("Bad things!");
+    console.log(error.message);
+  });
+
+// solving tests begin
+
+//test 1
+
+const submitData = (userName, userEmail) => {
+  return {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({ name: userName, email: userEmail }),
+  };
+};
+
+fetch("http://localhost:3000/users", submitData("jak", "jak.email.com"))
+  .then((response) => response.json())
+  .then((object) => console.log(object)); 
+
+// alternate way of writing code above. Passing an anonymous object as the second argument to fetch()
+
+// fetch("http://localhost:3000/dogs", {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify({
+//     dogName: "Byron",
+//     dogBreed: "Poodle",
+//   }),
+// });
